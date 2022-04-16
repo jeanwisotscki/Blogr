c935c4593005a19275805cad1853757c03a95dc8
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-04-16 10:09:41 -0300

    inicio script

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const hamburg = document.querySelector(".__menu-mobile");
+const nav = document.querySelector(".nav");
+
+hamburg.addEventListener("click", () => {
+  nav.style.display = "block";
+});
